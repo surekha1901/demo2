e93d9bf10d84cae0db140fd3be1b5f8298613679
surekha1901 <surekha.barfa@gmail.com>
2019-09-12 08:17:03 +0530

    my project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServersComponent } from './servers/servers.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './users/user/user.component';
+import { ServerComponent } from './servers/server/server.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { Route, RouterModule, Routes } from '@angular/router';
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'users', component: UserComponent},
+  {path: 'servers', component: ServerComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServersComponent,
+    UsersComponent,
+    UserComponent,
+    ServerComponent,
+    EditServerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
